59287ee1f66cbbfa31ca7a129d692a11a88f21d9
Mario Dietner <mario@next-sight.com>
2025-08-03 22:32:12 +0200

    fixing service principal role assignment, and changed policy to require-builting policies

--- modules/governance/azure_policy/main.tf
+++ modules/governance/azure_policy/main.tf
@@ -1,11 +1,21 @@
 resource "azurerm_subscription_policy_assignment" "assign_tag_resources_policy" {
-  name                 = "assign-tag-resources-policy"
+  name                 = "require-a-tag-on-resources"
   policy_definition_id = data.azurerm_policy_definition_built_in.tag_resoruces_policy.id
   subscription_id      = var.subscription_id
+  parameters = jsonencode({
+      "tagName" = {
+      "value" = "environment"
+      }
+    })
 }
 
 resource "azurerm_subscription_policy_assignment" "assign_tag_resourcegroup_policy" {
-  name                 = "assign-tag-resourcegroup-policy"
+  name                 = "require-a-tag-on-resource-groups"
   policy_definition_id = data.azurerm_policy_definition_built_in.tag_resourcegroup_policy.id
   subscription_id      = var.subscription_id
+  parameters = jsonencode({
+    "tagName" = {
+      "value" = "environment"
+    }
+  })
 }
